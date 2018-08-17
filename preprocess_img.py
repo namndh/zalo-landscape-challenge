6a3a89e306ac2dfdd4b7ad73e17c372179f2653c
t3min4l <hainam.nguyen.ict@gmail.com>
2018-08-17 22:28:27 +0700

    update

--- preprocess_img.py
+++ preprocess_img.py
@@ -6,6 +6,7 @@ from random import shuffle
 import numpy as np 
 import tables
 import h5py
+import pickle
 
 import constants	
 
@@ -16,7 +17,7 @@ PREPROCESSED_DATA_PATH = os.path.join(constants.DATA_DIR, 'preprocessed_data')
 PREPROCESSED_TRAIN_VAL_DATA_PATH = os.path.join(PREPROCESSED_DATA_PATH, 'train_val_data')
 PREPOCESSED_TEST_DATA_PATH = os.path.join(PREPROCESSED_DATA_PATH, 'test_data')
 HDF5_PATH = os.path.join(constants.DATA_DIR, 'trainval_data.hdf5')
-
+TRAIN_ADDRS_TEXT = os.path.join(constants.PROJECT_DIR, 'train_addrs.b')
 trainval_dataset = list()
 test_dataset = list()
 shuffle_dataset = True
@@ -67,43 +68,16 @@ val_labels = labels[int(0.8*len(addrs)):int(len(addrs))]
 train_shape = (len(train_addrs), 1, 480, 480)
 val_shape = (len(val_addrs), 1, 480, 480)
 
-hdf5_file = h5py.File(HDF5_PATH, mode='w')
-hdf5_file.create_dataset("train_imgs", train_shape, np.uint8)
-hdf5_file.create_dataset("val_imgs", val_shape, np.uint8)
+with open(TRAIN_ADDRS_TEXT, 'wb+') as f:
+	pickle.dump(train_addrs, f)
 
-hdf5_file.create_dataset("train_mean", train_shape[1:], np.float32)
 
-hdf5_file.create_dataset("train_labels", (len(train_addrs),), np.int8)
-hdf5_file["train_labels"][...] = train_labels
-hdf5_file.create_dataset("val_labels", (len(val_addrs),), np.int8)
-hdf5_file["val_labels"][...] = val_labels
 
-mean = np.zeros(train_shape[1:], np.float32)
-
-for i in range(len(train_addrs)):
-    # print how many images are saved every 1000 images
-    if i % 1000 == 0 and i > 1:
-    	print('Train data:{}/{}'.format(i, len(train_addrs)))
-
-    addr = train_addrs[i]
-    img = cv2.imread(addr)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    img = cv2.resize(img, (480, 480), interpolation=cv2.INTER_CUBIC)
-    
-    hdf5_file["train_imgs"][i, ...] = img[None]
-    mean += img / float(len(train_labels))
-
-for i in range(len(val_addrs)):
-    # print how many images are saved every 1000 images
-    if i % 1000 == 0 and i > 1:
-    	print('Val data:{}/{}'.format(i, len(val_addrs)))
-
-    addr = val_addrs[i]
-    img = cv2.imread(addr)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    img = cv2.resize(img, (480, 480), interpolation=cv2.INTER_CUBIC)
-
-    hdf5_file["val_imgs"][i, ...] = img[None]
-
-hdf5_file["train_mean"][...] = mean
-hdf5_file.close()
+# data = np.empty((len(train_addrs), 1, 480, 480), dtype=np.uint8)
+# for i, addr in enumerate(train_addrs):
+# 	if i % 5000 == 0 and i > 1:
+# 		print("Train data:{}/{}".format(i, len(train_addrs)))
+# 	img = cv2.imread(addr)
+# 	img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# 	img = cv2.resize(img, (480, 480), interpolation=cv2.INTER_CUBIC)
+# 	data[i, ...] = img[None]
--- test.py
+++ test.py
@@ -1,8 +1,54 @@
-i = 103 
-labels = [i] * 3
-print(labels)
+import os 
+import sys 
+import cv2
+import numpy as np 
+import pickle	
+import h5py
 
-import torch 
+import constants
 
-a = torch.ones(3,2,3,2) # size, chanel, H, W
-print(a)
+TRAIN_ADDRS_TEXT = os.path.join(constants.PROJECT_DIR, 'train_addrs.b')
+DATA_SET_PATH = os.path.join(constants.PROJECT_DIR, 'train_data.b')
+HDF5_PATH = os.path.join(constants.DATA_DIR, 'trainval_data.hdf5')
+
+with open(TRAIN_ADDRS_TEXT, 'rb') as f_in:
+	train_addrs = pickle.load(f_in)
+
+# data = np.empty((len(train_addrs), 1, 480, 480), dtype=np.uint8)
+# for i, addr in enumerate(train_addrs):
+# 	if i % 5000 == 0 and i > 1:
+# 		print("Train data:{}/{}".format(i, len(train_addrs)))
+# 	img = cv2.imread(addr)
+# 	img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# 	img = cv2.resize(img, (480, 480), interpolation=cv2.INTER_CUBIC)
+# 	data[i, ...] = img[None]
+# with open(DATA_SET_PATH, 'wb+') as f:
+# 	pickle.dump(data, f)
+# print(data.shape)
+# print(data[1, ...])
+# cv2.imshow(data[1, ...], 'img')
+# cv2.waitKey(2000)
+# cv2.destroyAllWindows()
+# with open(DATA_SET_PATH, 'wb+') as f:
+# 	pickle.dump(data, f)
+train_shape = (len(train_addrs), 1, 256, 256)
+hdf5_file = h5py.File(HDF5_PATH, mode='w')
+hdf5_file.create_dataset("train_img", train_shape, np.int8)
+hdf5_file.create_dataset("train_mean", train_shape[1:], np.float32)
+
+
+# mean = np.zeros(data_shape[1:], np.float32)
+
+for i in range(len(train_addrs)):
+	if i % 5000 == 0 and i > 1:
+		print('Train data: {}/{}'.format(i, len(train_addrs)))
+	addr = train_addrs[i]
+	img = cv2.imread(addr)
+	img = cv2.resize(img, (256, 256), interpolation=cv2.INTER_CUBIC)
+	img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+	hdf5_file["train_img"][i, ...] = img[None]
+	# mean += img / float(len(train_labels))
+
+# hdf5_file["train_mean"][...] = mean
+hdf5_file.close()
